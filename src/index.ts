90c9865fb11ca86d0019eae90658f9b4e482770b
william vegas <elgeokareem@pop-os.localdomain>
2021-12-23 11:52:00 -0400

    fixed eslint and prettier, added cors and env files support

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+// Init config
+dotenv.config();
 
 const app: express.Application = express();
+app.use(cors());
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Server running");
